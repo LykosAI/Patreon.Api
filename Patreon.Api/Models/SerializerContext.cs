479142f726cf32c13a8949e5c24f0e649d0708af
Ionite <dev@ionite.io>
2024-10-10 01:24:40 -0400

    Fix missing JsonSerializable

--- Patreon.Api/Models/SerializerContext.cs
+++ Patreon.Api/Models/SerializerContext.cs
@@ -1,7 +1,10 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Patreon.Api.Models;
 
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
 [JsonSerializable(typeof(PatreonIdentityResponse))]
+[JsonSerializable(typeof(JsonObject))]
+[JsonSerializable(typeof(JsonValue))]
 internal partial class PatreonApiSourceGenerationContext : JsonSerializerContext;
