ac94f58ca5249405fe6cd68ecf15895e14e19017
Ionite <dev@ionite.io>
2024-10-09 23:04:08 -0400

    Add IPatreonApi Json return overload and fields param

--- Patreon.Api/IPatreonApi.cs
+++ Patreon.Api/IPatreonApi.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Text.Json.Nodes;
+using JetBrains.Annotations;
 using Patreon.Api.Authorization;
 using Patreon.Api.Models;
 using Refit;
@@ -8,6 +9,12 @@ namespace Patreon.Api;
 [PublicAPI]
 public interface IPatreonApi
 {
+    private const string DefaultInclude = "memberships.campaign,memberships.currently_entitled_tiers";
+    private const string DefaultUserFields =
+        "email,first_name,full_name,image_url,last_name,thumb_url,url,vanity,is_email_verified";
+    private const string DefaultMemberFields =
+        "currently_entitled_amount_cents,lifetime_support_cents,campaign_lifetime_support_cents,last_charge_status,patron_status,last_charge_date,pledge_relationship_start,pledge_cadence";
+
     [Headers("Content-Type: application/x-www-form-urlencoded")]
     [Post("/oauth2/token")]
     Task<PatreonTokenResponse> GetToken(
@@ -17,7 +24,18 @@ public interface IPatreonApi
 
     [Get("/oauth2/v2/identity")]
     Task<PatreonIdentityResponse> GetIdentity(
-        [Query] string include = "memberships.campaign,memberships.currently_entitled_tiers",
+        string? include = DefaultInclude,
+        [AliasAs("fields[user]")] string? userFields = DefaultUserFields,
+        [AliasAs("fields[member]")] string? memberFields = DefaultMemberFields,
+        [Property(nameof(AuthorizationTokens))] AuthorizationTokens? authorization = null,
+        CancellationToken cancellationToken = default
+    );
+
+    [Get("/oauth2/v2/identity")]
+    Task<JsonObject> GetIdentityJson(
+        string? include = DefaultInclude,
+        [AliasAs("fields[user]")] string? userFields = DefaultUserFields,
+        [AliasAs("fields[member]")] string? memberFields = DefaultMemberFields,
         [Property(nameof(AuthorizationTokens))] AuthorizationTokens? authorization = null,
         CancellationToken cancellationToken = default
     );
